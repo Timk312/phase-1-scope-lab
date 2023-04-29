e42f5962b1b3f5a1a9dffee0a1b50e7eb9b8146c
Tim Kingston <timkingston312@gmail.com>
2023-04-29 11:33:54 -0400

    scope

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName='bob';
+const leastFavoriteCustomer="some";
+function upperCaseCustomerName(){
+customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+ bestCustomer='not bob';
+}
+bestCustomer;
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob';
+}
+bestCustomer;
+
+
+
+function changeLeastFavoriteCustomer(){
+leastFavoriteCustomer="another bob";
+}
